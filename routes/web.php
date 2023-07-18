1940bf6285a6e1507799ef53cbb686691953f6db
Ericson-Manalo <ericmana98@gmail.com>
2023-07-18 16:15:34 +0200

    created another item beyond 'element' called 'ref', which call the route of the file created

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,35 @@ Route::get('/', function () {
     $data = [
         'listItems' => [
             [
+                'ref' => 'route("home")',
                 'element' => 'Home',
             ],
-            [
+            [   
+                'ref' => 'route("download")',
                 'element' => 'Scarica',
             ],
             [
+                'ref' => 'route("why")',
                 'element' => 'Perché Discord',
             ],
             [
+                'ref' => 'route("nitro")',
                 'element' => 'Nitro',
             ],
             [
+                'ref' => 'route("security")',
                 'element' => 'Sicurezza',
             ],
-            [
+            [   
+                'ref' => 'route("assistance")',
                 'element' => 'Assistenza',
             ]
         ]
     ];
     return view('home', $data);
 });
+
+Route::get('/download', function () {
+
+    return view('download', $data);
+})->name('download');
