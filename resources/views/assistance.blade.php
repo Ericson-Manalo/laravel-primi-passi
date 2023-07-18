fdb19d268179ae203c70cecaaba7bc837e066372
Ericson-Manalo <ericmana98@gmail.com>
2023-07-18 17:59:42 +0200

    gave title to each file created

---
+++ resources/views/assistance.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assistance</title>
+</head>
+<body>
+    <h1>
+        Welcome to the section <em>Assistance</em> !!!!
+    </h1>
+</body>
+</html>
